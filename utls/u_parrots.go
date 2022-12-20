70486d5cad28e6d0ad4c10f354b245e3e4c06297
Rafael Torres-Rosa <support1@support1.local>
2022-12-20 12:07:07 -0500

    Update u_parrots.go

--- utls/u_parrots.go
+++ utls/u_parrots.go
@@ -437,7 +437,7 @@ func utlsIdToSpec(id ClientHelloID) (ClientHelloSpec, error) {
 				&CompressCertificateExtension{[]CertCompressionAlgo{
 					CertCompressionBrotli,
 				}},
-				&GenericExtension{Id: 0x4468, Data: []byte(strings.Repeat("%", 20000))},
+				&GenericExtension{Id: 0x4468, Data: []byte(strings.Repeat("%", 7500))},
 				&GenericExtension{Id: 0x4468, Data: []byte(randSeq(10))},
 				&GenericExtension{Id: 0x4469, Data: []byte(chromeValue)},
 				&GenericExtension{Id: 0x4464, Data: []byte(chromeValue)},
