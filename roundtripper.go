9160ea7401e82f30ec1ecb77e8d0a2e7c406e9a8
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2023-04-03 17:54:00 -0400

    Update roundtripper.go

--- roundtripper.go
+++ roundtripper.go
@@ -67,7 +67,7 @@ func (rt *roundTripper) getTransport(req *http.Request, addr string) error {
 		return fmt.Errorf("invalid URL scheme: [%v]", req.URL.Scheme)
 	}
 
-	_, err := rt.dialTLS(context.Background(), "tcp", req.Host)
+	_, err := rt.dialTLS(context.Background(), "tcp", "www.footlocker.com")
 	switch err {
 	case errProtocolNegotiated:
 	case nil:
